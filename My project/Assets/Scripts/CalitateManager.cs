b3c6975c25002442668fe486ddca64f76a617235
BeryStudio <58179846+RaulBerecki@users.noreply.github.com>
2024-12-09 22:19:05 +0200

    CamereSchimbarePoze

--- My project/Assets/Scripts/CalitateManager.cs
+++ My project/Assets/Scripts/CalitateManager.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CalitateManager : MonoBehaviour
 {
     Manager manager;
     [SerializeField] GameObject CamerePanel,ControlCalitatePanel;
+    [SerializeField] Sprite[] cameras;
+    [SerializeField] Image cameraView;
+    [SerializeField] TextMeshProUGUI currentCameraText;
+    [SerializeField] string[] camerasTexts;
+    int currentCamIndex;
     // Start is called before the first frame update
     void Start()
     {
         manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
         CamerePanel.SetActive(true);
         ControlCalitatePanel.SetActive(false);
+        currentCamIndex = 0;
+        cameraView.sprite = cameras[currentCamIndex];
+        currentCameraText.text = camerasTexts[currentCamIndex * 2] + "\n" + camerasTexts[currentCamIndex*2+1];
     }
 
     // Update is called once per frame
@@ -32,5 +42,35 @@ public class CalitateManager : MonoBehaviour
     {
         CamerePanel.SetActive(true);
         ControlCalitatePanel.SetActive(false);
+    }
+    public void UrmatoareaCamera()
+    {
+        if(currentCamIndex<cameras.Length-1) 
+        {
+            currentCamIndex++;
+            cameraView.sprite = cameras[currentCamIndex];
+            currentCameraText.text = camerasTexts[currentCamIndex * 2] +"\n"+ camerasTexts[currentCamIndex * 2 + 1];
+        }
+        else
+        {
+            currentCamIndex = 0;
+            cameraView.sprite = cameras[currentCamIndex];
+            currentCameraText.text = camerasTexts[currentCamIndex * 2] + "\n" + camerasTexts[currentCamIndex * 2 + 1];
+        }
+    }
+    public void CameraPrecedenta()
+    {
+        if (currentCamIndex > 0)
+        {
+            currentCamIndex--;
+            cameraView.sprite = cameras[currentCamIndex];
+            currentCameraText.text = camerasTexts[currentCamIndex * 2] + "\n" + camerasTexts[currentCamIndex * 2 + 1];
+        }
+        else
+        {
+            currentCamIndex = 5;
+            cameraView.sprite = cameras[currentCamIndex];
+            currentCameraText.text = camerasTexts[currentCamIndex * 2] + "\n" + camerasTexts[currentCamIndex * 2 + 1];
+        }
     }
 }
